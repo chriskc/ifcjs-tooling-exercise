e26ce9ffa709de9a43193269ad9fb339ddd74728
chriskc <chriskelvinchan@gmail.com>
2023-07-31 18:00:12 -0700

    init rollup & resolved package dependencies error

---
+++ index.js
@@ -0,0 +1,18 @@
+// import * as THREE from 'three'
+
+import { Color } from 'three';
+import { IfcViewerAPI } from 'web-ifc-viewer';
+// import { IfcViewerAPI } from 'ifc-viewer-api';
+
+const container = document.getElementById('viewer-container');
+const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
+viewer.grid.setGrid();
+viewer.axes.setAxes();
+
+async function loadIfc(url) {
+    // await viewer.IFC.setWasmPath("./");
+    const model = await viewer.IFC.loadIfcUrl(url);
+    viewer.shadowDropper.renderShadow(model.modelID);
+}
+
+loadIfc("model-house.ifc");
